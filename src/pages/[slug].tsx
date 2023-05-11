400144c5c2aa1a352ae6f4987e54f48b877cb9d8
jbokyes <joaquinbezanilla@gmail.com>
2023-05-10 20:43:05 -0400

    Added styling to profile page

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -13,7 +13,20 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
         <title>{data.username}</title>
       </Head>
       <PageLayout>
-        <div>{data.username}</div>
+        <div className="relative h-36 bg-slate-600">
+          <Image
+            src={data.profilePicture}
+            alt={`${data.username ?? ""}'s profile pic`}
+            width={128}
+            height={128}
+            className="border-slate absolute bottom-0 left-0 -mb-[64px] ml-4 rounded-full border-4 bg-black"
+          />
+        </div>
+        <div className="h-[64px]"></div>
+        <div className="p-4 text-2xl font-semibold">{`@${
+          data.username ?? ""
+        }`}</div>
+        <div className="w-full border-b border-slate-400"></div>
       </PageLayout>
     </>
   );
@@ -23,6 +36,7 @@ import { appRouter } from "~/server/api/root";
 import { prisma } from "~/server/db";
 import superjson from "superjson";
 import { PageLayout } from "~/components/layout";
+import Image from "next/image";
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const ssg = createServerSideHelpers({
